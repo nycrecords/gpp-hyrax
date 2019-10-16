d84fef83b1d9c0415c0d67a4dc585e94474062b3
johnyu95 <jyu@records.nyc.gov>
2019-10-16 16:47:54 -0400

    Created comment notification in workflow and hid comment UI from non admins

---
+++ app/services/hyrax/workflow/comment_notification.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Hyrax
+  module Workflow
+    class CommentNotification < GppNotification
+      def workflow_recipients
+        { to: library_reviewers }
+      end
+
+      def subject
+        'You have a new comment'
+      end
+
+      def message
+        "#{user.display_name} (#{user.user_key}) has made a new comment on '#{title}' (#{link_to work_id, document_url})<br><br>" +
+            "Comment: #{comment}<br><br>" +
+            "To view all comments on this submission, please click here: #{link_to work_id, document_url}"
+      end
+    end
+  end
+end
