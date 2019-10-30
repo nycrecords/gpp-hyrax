73ac3af63c6b29e101204fa6dc43fccab1ec8aac
Joel Castillo <jocastillo@records.nyc.gov>
2019-10-30 13:00:18 -0400

    Revert changes to omniauth_controller.rb

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -2,8 +2,4 @@ class OmniauthController < Devise::SessionsController
   def new
     redirect_to user_saml_omniauth_authorize_path
   end
-
-  def profile
-    redirect_to ENV['NYC_ID_WEB_SERVICES_URL']
-  end
 end
