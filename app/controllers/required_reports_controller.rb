c4d9a6566b3b21c2f73a5622d692253d38647599
Gary Zhou <gzhou@records.nyc.gov>
2019-11-01 10:41:27 -0400

    Removed edit view for required_reports and comment edit/delete functionality in controller

--- app/controllers/required_reports_controller.rb
+++ app/controllers/required_reports_controller.rb
@@ -18,8 +18,8 @@ class RequiredReportsController < ApplicationController
   end
 
   # GET /required_reports/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /required_reports
   # POST /required_reports.json
@@ -39,27 +39,27 @@ class RequiredReportsController < ApplicationController
 
   # PATCH/PUT /required_reports/1
   # PATCH/PUT /required_reports/1.json
-  def update
-    respond_to do |format|
-      if @required_report.update(required_report_params)
-        format.html { redirect_to @required_report, notice: 'Required report was successfully updated.' }
-        format.json { render :show, status: :ok, location: @required_report }
-      else
-        format.html { render :edit }
-        format.json { render json: @required_report.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @required_report.update(required_report_params)
+  #       format.html { redirect_to @required_report, notice: 'Required report was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @required_report }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @required_report.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /required_reports/1
   # DELETE /required_reports/1.json
-  def destroy
-    @required_report.destroy
-    respond_to do |format|
-      format.html { redirect_to required_reports_url, notice: 'Required report was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @required_report.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to required_reports_url, notice: 'Required report was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
