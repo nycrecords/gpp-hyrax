32f6fb99cb080ab910a7654fd81c69c148c48c8f
Gary Zhou <gzhou@records.nyc.gov>
2019-11-06 13:38:36 -0500

    Add Agencies table and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191106144533) do
+ActiveRecord::Schema.define(version: 20191106182840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "agencies", force: :cascade do |t|
+    t.string "name"
+    t.string "point_of_contact_emails", default: [], array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "bookmarks", id: :serial, force: :cascade do |t|
     t.integer "user_id", null: false
