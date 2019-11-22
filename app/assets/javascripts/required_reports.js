457ccc5e69eef7b3db4b8cbeb5fcfdb289eccfcf
Gary Zhou <gzhou@records.nyc.gov>
2019-11-22 11:03:40 -0500

    Remove frequency integer requirement if frequency is 'Once'

--- app/assets/javascripts/required_reports.js
+++ app/assets/javascripts/required_reports.js
@@ -22,6 +22,7 @@ $(document).ready(function () {
     $('#new_required_report').submit(function (e) {
         var local_law_value_length = $('#required_report_local_law').val().length,
             charter_and_code_value_length = $('#required_report_charter_and_code').val().length,
+            frequency_value = $('#required_report_frequency').val(),
             frequency_value_length = $('#required_report_frequency').val().length,
             frequency_integer_value_length = $('#required_report_frequency_integer').val().length,
             other_frequency_description_value_length = $('#required_report_other_frequency_description').val().length,
@@ -39,15 +40,16 @@ $(document).ready(function () {
 
         // other_frequency_description is empty
         if (other_frequency_description_value_length === 0) {
-            // One of frequency or frequency_integer is empty
-            if (frequency_value_length === 0 || frequency_integer_value_length === 0 || start_date === 0) {
+            // One of frequency or frequency_integer (if frequency is not Once) or start date is empty
+            if (frequency_value_length === 0
+                || (frequency_value !== 'Once' && frequency_integer_value_length === 0)
+                || start_date === 0) {
                 errorDiv.text('Frequency, Frequency Integer and Start Date or Other frequency description is required.');
                 errorDiv.show();
                 errorDiv.focus();
 
                 return false;
             }
-
         } else { // other_frequency_description is not empty
             // One of frequency or frequency_integer is not empty
             if (frequency_value_length > 0 || frequency_integer_value_length > 0) {
