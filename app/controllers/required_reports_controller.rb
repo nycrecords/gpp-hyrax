22dc9bae9ae165980ddc52023971bc760c19f042
johnyu95 <jyu@records.nyc.gov>
2019-11-25 16:18:07 -0500

    Created public list template for required reports

--- app/controllers/required_reports_controller.rb
+++ app/controllers/required_reports_controller.rb
@@ -74,6 +74,11 @@ class RequiredReportsController < ApplicationController
     render json: { 'report_names': @report_names }
   end
 
+  # GET /required_reports/public_list
+  def public_list
+    @required_reports = RequiredReport.all
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_required_report
