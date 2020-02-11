784d12f67e31b438d3ee409e1af86d19529c9450
johnyu95 <jyu@records.nyc.gov>
2020-02-11 10:22:55 -0500

    Added virus scanning using uvscan

--- lib/mcafee_virus_scan_linux.rb
+++ lib/mcafee_virus_scan_linux.rb
@@ -2,16 +2,17 @@ class McafeeVirusScanLinux < Hydra::Works::VirusScanner
 
   # @param file [String]
   # @param uvscan_path [String]
-  def initialize(file, uvscan_path: "/usr/local/bin/uvscan")
-    super
+  def initialize(file, uvscan_path: '/usr/local/bin/uvscan')
+    @file = file
     @uvscan_path = uvscan_path
   end
 
   def infected?
-    scan_command = "#{@uvscan_path} -c -v #{file}"
+    scan_command = "#{@uvscan_path} --delete -v #{file.path}"
     scan_result = `#{scan_command}`
-    return false if scan_result == 0
-    warning "A virus was found in #{file}: #{scan_result}"
+    file_exists = Dir.glob(file.path)
+    return false unless file_exists.empty?
+    warning "A virus was found in #{file.path}"
     true
   end
 end
