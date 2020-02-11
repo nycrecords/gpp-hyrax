e007b7751956fdd5bbd4f0b46b77820287d6a131
Joel Castillo <jocastillo@records.nyc.gov>
2020-02-11 15:22:06 +0000

    Initial changes for deployment

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,8 @@
+require 'dotenv'
+
+Dotenv.load('/opt/hyrax/.env')
+
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
