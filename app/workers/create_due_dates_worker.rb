766bbc265225b24dc0434833c0330e5f0e0a09e0
Gary Zhou <gzhou@records.nyc.gov>
2021-05-13 11:29:14 -0400

    Updates for production migration

--- app/workers/create_due_dates_worker.rb
+++ app/workers/create_due_dates_worker.rb
@@ -8,7 +8,9 @@ class CreateDueDatesWorker
                          .where("end_date is null OR end_date >= ?", current_date)
     required_reports.each do |rr|
       # Get most recent year to determine start_date and calculate future due dates
-      latest_year = RequiredReportDueDate.where(required_report_id: rr.id).order(base_due_date: :desc).first.base_due_date.year
+      rr_due_date = RequiredReportDueDate.where(required_report_id: rr.id).order(base_due_date: :desc).first
+
+      latest_year = rr_due_date.nil? ? rr.start_date.year : rr_due_date.base_due_date.year
       return unless (latest_year - current_date.year) <= 1
 
       start_date_month = rr.start_date.month
--- db/migrate/20210505160641_add_automated_date_to_required_reports.rb
+++ db/migrate/20210505160641_add_automated_date_to_required_reports.rb
@@ -1,7 +1,14 @@
 class AddAutomatedDateToRequiredReports < ActiveRecord::Migration[5.1]
   def change
-    add_column :required_reports, :automated_date, :boolean, null: false, default: false
-    add_column :required_report_due_dates, :grace_due_date, :date, null: false
+    add_column :required_reports, :automated_date, :boolean, null: false, default: true
     rename_column :required_report_due_dates, :due_date, :base_due_date
+
+    # Set grace_due_date to base_due_date
+    add_column :required_report_due_dates, :grace_due_date, :date
+    RequiredReportDueDate.find_each do |rr|
+      rr.update_attributes!(:grace_due_date => rr.base_due_date)
+    end
+
+    change_column_null :required_report_due_dates, :grace_due_date, false
   end
 end
