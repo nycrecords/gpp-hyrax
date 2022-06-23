9a505c50a5260d9a54945748f219cbd5d5f12952
Administrator <administrator@mbpr-105-0601pm.csc.nycnet>
2022-06-23 10:05:24 -0400

    adding few more bulkrax related files

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# App::Application.config.session_store :cookie_store, 
+#                                          :key => "_yourapp_session",
+#                                          :expire_after => 2.minutes
