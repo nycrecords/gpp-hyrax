c43f504024dd1f25cebda26d285bbff2ff0ba329
Gary Zhou <gzhou@records.nyc.gov>
2024-01-30 11:55:50 -0500

    Add changes from production

--- app/controllers/concerns/hyrax/works_controller_behavior.rb
+++ app/controllers/concerns/hyrax/works_controller_behavior.rb
@@ -167,7 +167,7 @@ module Hyrax
       # If there are no previous publications, set date_published to nil
       if publications.present?
         publications.each do |p|
-          if !(p.suppressed?) && required_report
+          if !(p.suppressed?) && p.required_report_name != "Not Required"
             # Set required_report.date_published to the publication with the next chronological published date
             required_report.update_attributes(last_published_date: p.date_published)
             break
